e586b5f0e677830b8d5ea0b90a6074a56aef138b
niroshkumarh <64655849+niroshkumarh@users.noreply.github.com>
2022-09-04 04:26:39 -0700

    Update simplyCountdown.js
    date changws

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -116,8 +116,8 @@
     simplyCountdown = function (elt, args) {
         var parameters = extend({
                 year: 2022,
-                month: 6,
-                day: 9,
+                month: 9,
+                day: 6,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
